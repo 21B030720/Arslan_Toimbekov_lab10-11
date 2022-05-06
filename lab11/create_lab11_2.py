c847cba721c6ca00882632da45a602eb3fe90b68
21B030720 <98741201+21B030720@users.noreply.github.com>
2022-05-06 21:32:44 +0600

    Add files via upload

---
+++ lab11/create_lab11_2.py
@@ -0,0 +1,30 @@
+#searching = str(searching)
+import psycopg2
+host = "127.0.0.1"
+user = "postgres"
+password = "Loli0"
+db_name = "postgres"
+
+try:
+    connection = psycopg2.connect(
+        host=host,
+        user=user,
+        password=password,
+        database=db_name
+    )
+    connection.autocommit = True
+    with connection.cursor() as cursor:
+        cursor.execute("""CREATE TABLE telephone(
+                 name varchar(50) NOT NULL,
+                 surname varchar(50) NOT NULL,
+                 number int PRIMARY KEY NOT NULL);""")
+        print("Succesfully")
+        cursor.execute("select * from telephone")
+        result = cursor.fetchall()
+        print(result, "Good")
+except Exception as ex:
+    print("Error while workig with PostgreSQL", ex)
+finally:
+    if connection:
+        connection.close()
+        print("PostgreSQL connection closed")
